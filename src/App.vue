132bc9319bd79eda5a4db0f608fd2ac951a9a0c6
Yunandtidus <bruno.wintenberger@gmail.com>
2020-10-29 21:53:21 +0100

    tmp

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        Workout
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <WorkoutConfigurer/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from 'vue';
+import WorkoutConfigurer from './components/WorkoutConfigurer.vue';
 
-#nav {
-  padding: 30px;
+export default Vue.extend({
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    WorkoutConfigurer,
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/components/WorkoutConfigurer.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+      <v-form>
+          <v-text-field type="numeric" label="Durée" v-model="time"/>
+            <div class="text-center">
+                <v-chip 
+                    v-for="cat in categories" 
+                    :color="cat.selected ? 'blue' : ''"
+                    class="ma-2"
+                    :key="cat.name"
+                    @click="cat.selected = !cat.selected">
+                    {{ cat.name }}
+                </v-chip>
+               </div>
+      </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            time : 0,
+            categories : [
+                {name : 'Cardio', selected : true},
+                {name : 'Pectoraux', selected : true},
+                {name : 'Abdos', selected : true},
+                {name : 'Jambes', selected : true},
+                {name : 'Bras', selected : true},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style> 
